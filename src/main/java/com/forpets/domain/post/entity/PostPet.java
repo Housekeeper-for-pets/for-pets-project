bf933567fcf4b7c4fb8c34ecb1c60939904f8f3d
kwonjiwon <kjw81024@gmail.com>
2026-05-16 23:18:31 +0900

    feat: initial entity with embedded...

---
+++ src/main/java/com/forpets/domain/post/entity/PostPet.java
@@ -0,0 +1,35 @@
+package com.forpets.domain.post.entity;
+
+import com.forpets.domain.pet.entity.Pet;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "post_pet")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostPet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Column(name = "pet_id", nullable = false)
+    private Long petId;
+
+    @Embedded
+    private PetSnapshot petSnapshot;
+
+    public static PostPet createFrom(Long postId, Pet pet) {
+        PostPet postPet = new PostPet();
+        postPet.postId = postId;
+        postPet.petId = pet.getId();
+        postPet.petSnapshot = PetSnapshot.from(pet);
+        return postPet;
+    }
+}
