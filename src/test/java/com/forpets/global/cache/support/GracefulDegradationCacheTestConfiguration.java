a6f34cee8a5a7808fcc068037b2794d352f8b73f
“grown-tree” <tjsruddks12@naver.com>
2026-05-27 15:18:15 +0900

    fix : pr 에러 수정

--- src/test/java/com/forpets/global/cache/support/GracefulDegradationCacheTestConfiguration.java
+++ src/test/java/com/forpets/global/cache/support/GracefulDegradationCacheTestConfiguration.java
@@ -1,19 +1,19 @@
 package com.forpets.global.cache.support;
 
 import com.forpets.global.cache.GracefulDegradationCacheErrorHandler;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 /**
  * Graceful Degradation 통합 테스트용 캐시 설정.
  * Redis 없이 실패하는 CacheManager + CacheErrorHandler를 주입합니다.
  */
-@Configuration
+@TestConfiguration
 @EnableCaching
 public class GracefulDegradationCacheTestConfiguration implements CachingConfigurer {
 
