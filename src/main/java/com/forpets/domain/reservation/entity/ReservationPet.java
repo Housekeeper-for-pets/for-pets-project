0704f0331c6e51091b505f2a3b97721066b884f9
yspark <youngsue5153@gmail.com>
2026-05-29 10:53:25 +0900

    refac:

--- src/main/java/com/forpets/domain/reservation/entity/ReservationPet.java
+++ src/main/java/com/forpets/domain/reservation/entity/ReservationPet.java
@@ -1,10 +1,8 @@
 package com.forpets.domain.reservation.entity;
 
 import com.forpets.global.embed.entity.PetSnapshot;
-import com.forpets.global.entity.CreatedEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
